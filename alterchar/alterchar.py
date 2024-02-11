5da05626cd99076d403887e92bcd7f2b97b1ed4e
Xhi3r <aphilakjankaewdach@gmail.com>
2024-02-11 22:13:55 +0700

    add alterchar module

---
+++ alterchar/alterchar.py
@@ -0,0 +1,17 @@
+def alternatingCharacters(s):
+    delete = 0
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            delete += 1
+    return delete
+    
+if __name__ == '__main__':
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+        
+        print(result)
