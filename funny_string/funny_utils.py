f59234c3e0cd312091dda64e7d4d2e4a44cfa8dd
Xhi3r <aphilakjankaewdach@gmail.com>
2024-02-11 22:55:15 +0700

    add test funny

--- funny_string/funny_utils.py
+++ funny_string/funny_utils.py
@@ -1,4 +1,4 @@
-def isubin_funny_subintring(subin):
+def isubin_funny(subin):
     a=subin[::-1]
     for i in range(len(subin)-1):
         if abs(ord(subin[i])-ord(subin[i+1]))!=abs(ord(a[i])-ord(a[i+1])):
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     for qin_itr in range(qin):
         subin = input()
 
-        resubinult = isubin_funny_subintring(subin)
+        resubinult = isubin_funny(subin)
         print(resubinult + '\n')
